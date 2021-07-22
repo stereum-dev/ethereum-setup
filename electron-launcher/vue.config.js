8302517be278ab3e61bd56897a16f69399330a19
michaelferjancic <michaelferjancic@MacBook-Pro-von-michaelferjancic.local>
2021-07-22 20:26:56 +0200

    -Change: remove iconv

--- electron-launcher/vue.config.js
+++ electron-launcher/vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
     pluginOptions: {
         electronBuilder: {
           preload: 'src/preload.js',
-          externals: ['cpufeatures.node'],
+          externals: ['cpufeatures','sshcrypto','ssh2'],
         }
     },
     configureWebpack: {
