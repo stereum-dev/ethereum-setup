24efad1cadbe9f67a589b51a3311fe057cac6d02
stefa2k <stefan.kobrc@rocklogic.at>
2021-10-26 09:23:02 +0200

    fix build on intel-mac

--- electron-launcher/vue.config.js
+++ electron-launcher/vue.config.js
@@ -5,6 +5,7 @@ module.exports = {
     pluginOptions: {
         electronBuilder: {
           preload: 'src/preload.js',
+          externals: ['ssh2', 'tunnel-ssh'],
         }
     },
     transpileDependencies: [
